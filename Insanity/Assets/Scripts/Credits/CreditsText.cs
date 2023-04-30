c35a6c7ea772154606e6b61e2e8f22f89e333770
Celestino <45713831+Cell21@users.noreply.github.com>
2023-05-01 00:28:11 +0100

    Creditos

--- Insanity/Assets/Scripts/Credits/CreditsText.cs
+++ Insanity/Assets/Scripts/Credits/CreditsText.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsText : MonoBehaviour
 {
@@ -23,6 +24,11 @@ public class CreditsText : MonoBehaviour
         {
             float distance = moveSpeed * Time.deltaTime;
             textTransform.position += new Vector3(0f, distance, 0f);
+        }
+        else if(elapsedTime >= duration) 
+        {
+            //Go to boss scene
+            SceneManager.LoadScene(0);
         }
     }
 }
