731e18df147ee874eec2f9084dd6d7ed9ecc6e75
OMeninoPrecavido <andrecoccoconde20@gmail.com>
2023-04-30 16:51:40 +0100

    Tentando fazer movimento
    Socorro eu quero morrer

--- Insanity/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Insanity/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -4,36 +4,53 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private float speed = 300;
-    private Vector3 currDirection;
+    private const float slowSpeed = 300;
+    private const float fastSpeed = 300;
+    private const float rotateSpeed = 30;
+
+    [SerializeField] private Vector3 currDirection;
 
     private float stdMoveTimer = 0;
     private float stdMoveDuration = 2;
     private float stdStopDuration = 2.5f;
-    private bool stdIsMoving = false;
+    [SerializeField] private bool stdIsMoving = false;
+
+    private float alertRadius = 1000;
+    [SerializeField] private bool playerInRange = false;
 
     private Rigidbody rb;
+    [SerializeField] private GameObject player;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
     {
-        UpdateTimer();
+        CheckForPlayer();
 
-        if (stdIsMoving){
-            Move(currDirection);
-        } else {
-            rb.velocity = Vector3.zero;
+        if (playerInRange)
+        {
+            currDirection = player.transform.position - transform.position;
+            Move(currDirection, fastSpeed);
+        }
+        else
+        {
+            UpdateTimer();
+
+            if (stdIsMoving){
+                Move(currDirection, slowSpeed);
+            } else {
+                rb.velocity = Vector3.zero;
+            }
         }
     }
 
     private Vector3 GetRandomDir()
     {
         Vector2 newDir = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
-        newDir = newDir.normalized;
         return new Vector3(newDir.x, 0, newDir.y);
     }
 
@@ -46,9 +63,7 @@ public class EnemyMovement : MonoBehaviour
                 stdMoveTimer = 0;
                 stdIsMoving = false;
             }
-        }
-
-        if (!stdIsMoving) {
+        } else {
             if (stdMoveTimer >= stdStopDuration)
             {
                 stdMoveTimer = 0;
@@ -59,8 +74,25 @@ public class EnemyMovement : MonoBehaviour
         
     }
 
-    private void Move(Vector3 Dir)
+    private void Move(Vector3 Dir, float speed)
     {
-        rb.velocity = Dir * speed * Time.deltaTime;
+        Vector3 dir = Dir.normalized;
+        Debug.Log("Normalizado");
+        dir.y = 0;
+        //transform.forward = Vector3.Slerp(transform.forward, -dir, Time.deltaTime * rotateSpeed);
+        rb.velocity = dir * speed * Time.deltaTime;
+        Debug.Log("Me movi");
+    }
+
+    private void CheckForPlayer()
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 enemyPos = transform.position;
+        if ((enemyPos - playerPos).magnitude < alertRadius) {
+            playerInRange = true;
+            Debug.Log("achei");
+        } else {
+            playerInRange = false;
+        }
     }
 }
