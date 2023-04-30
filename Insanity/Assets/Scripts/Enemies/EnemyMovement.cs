cb0666bd1380056293abbb1521646fbf6e2ad3b3
OMeninoPrecavido <andrecoccoconde20@gmail.com>
2023-04-30 21:54:44 +0100

    Inimigo feio e chato e ruim
    movimentação random

--- Insanity/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Insanity/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -5,20 +5,58 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody rb;
-    private GameObject player;
 
     private float speed = 4;
+
+    private bool isMoving = false;
+    private float movementTimer = 0;
+    private float timeStopped = 0.5f;
+    private float timeMoving = 1f;
+
+    private Vector3 currDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        Vector3 direction = player.transform.position - rb.transform.position;
-        direction.Normalize();
-        rb.velocity = direction * speed;
+        TimerUpdate();
+
+        if (isMoving)
+        {
+            MoveEnemy();
+        }
+    }
+
+    private void TimerUpdate()
+    {
+        movementTimer += Time.deltaTime;
+
+        if (isMoving == true && movementTimer >= timeMoving)
+        {
+            isMoving = false;
+            movementTimer = 0;
+        }
+        
+        if (isMoving == false && movementTimer >= timeStopped)
+        {
+            isMoving = true;
+            movementTimer = 0;
+            currDirection = RandomDirection();
+        }
+    }
+
+    private Vector3 RandomDirection()
+    {
+        Vector3 randomVec = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+        return randomVec;
+    }
+
+    private void MoveEnemy()
+    {
+        Vector3 dir = currDirection.normalized;
+        rb.velocity = dir * speed;
     }
 }
