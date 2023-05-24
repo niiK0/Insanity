647dca0825907ed26c0434fd93b3a263d5a0a83d
niiK0 <37949374+niiK0@users.noreply.github.com>
2023-05-24 02:02:56 +0100

    boss? xD

--- Insanity/Assets/SimpleAIWalk.cs
+++ Insanity/Assets/SimpleAIWalk.cs
@@ -14,6 +14,7 @@ public class SimpleAIWalk : MonoBehaviour
         if (agent.isOnNavMesh)
         {
             agent.SetDestination(target.position);
+            transform.LookAt(target);
         }
     }
 }
